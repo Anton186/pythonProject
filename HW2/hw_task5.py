770368664c2b04e3b03028e140fb92a3774fa0fe
Toxa <black2604@mail.ru>
2022-11-27 18:19:46 +0500

    hw2 done!

--- HW2/hw_task5.py
+++ HW2/hw_task5.py
@@ -2,8 +2,9 @@
 # другие методы из библиотеки random - можно).
 import random
 sp =[1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+print("Начальный список: ", sp)
 for i in range(len(sp)):
     k = random.randint(0, len(sp)-1)
     z = random.randint(0, len(sp)-1)
     sp[k], sp[z] = sp[z], sp[k]
-print(sp)
+print("Перемешанный список: ", sp)
